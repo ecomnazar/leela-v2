c3a30076c498c60246621d3961897d64b44936e1
ecomnazar <ecomnazar.me@gmail.com>
2025-02-05 13:14:04 +0500

    [fixed]: redirect_uri

--- src/features/auth/ui/AuthModal.tsx
+++ src/features/auth/ui/AuthModal.tsx
@@ -21,6 +21,7 @@ export const AuthModal = () => {
     if (isFulfilled(response)) {
       console.log(window.Telegram.WebApp.initDataUnsafe.start_param);
       console.log(window.Telegram.WebApp.initDataUnsafe);
+      console.log(JSON.stringify(window.Telegram.WebApp));
 
       // const OAuthUrl = response.payload.url;
       // Linking.openURL(OAuthUrl);
