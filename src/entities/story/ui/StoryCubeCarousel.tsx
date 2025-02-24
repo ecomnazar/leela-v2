706bd3eea65cfcc5bec3623db29f09f647c1d1d5
ecomnazar <ecomnazar.me@gmail.com>
2025-02-24 13:58:57 +0500

    [fixing]: image flash & scroll x on mobile

--- src/entities/story/ui/StoryCubeCarousel.tsx
+++ src/entities/story/ui/StoryCubeCarousel.tsx
@@ -39,7 +39,7 @@ export const StoryCubeCarousel = () => {
       showsHorizontalScrollIndicator={false}
       onScroll={Animated.event(
         [{ nativeEvent: { contentOffset: { x: scrollX } } }],
-        { useNativeDriver: false }
+        { useNativeDriver: true }
       )}
       windowSize={5}
       maxToRenderPerBatch={2}
--- src/pages/(bottomNavbar)/main/ui/page.tsx
+++ src/pages/(bottomNavbar)/main/ui/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { CardsSection } from "./CardsSection";
 import { Stories } from "./Stories";
 import { Search } from "@/widgets/search";
-import { View } from "react-native";
+import { Text, View } from "react-native";
 import { PageHeader } from "@/widgets/pageHeader";
 import { useAuth } from "@/entities/auth/hooks/useAuth";
 import { FixedButton } from "./FixedButton";
@@ -10,6 +10,7 @@ import { useFilter } from "../hooks/useFilter";
 import { AnimatedContainer } from "./AnimatedContainer";
 import { Redirect, useLocalSearchParams } from "expo-router";
 import { CreateStoryModal } from "@/features/profile/modals/CreateStoryModal";
+import { Animated } from "react-native";
 
 export const MainPage = () => {
   useFilter();
@@ -25,6 +26,36 @@ export const MainPage = () => {
       <PageHeader enableEnergyShowcase enableBalanceShowcase disableBorder />
       <View style={{ flex: 1, marginTop: 10 }}>
         <Stories />
+        <Animated.FlatList
+          data={[1, 2, 3]}
+          keyExtractor={(item: number) => item.toString()}
+          horizontal
+          pagingEnabled
+          showsHorizontalScrollIndicator={false}
+          // onScroll={Animated.event(
+          //   [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+          //   { useNativeDriver: true }
+          // )}
+          // initialScrollIndex={currentStoryIndex}
+          scrollEventThrottle={16}
+          // getItemLayout={(_, index) => ({
+          //   length: WINDOW_WIDTH,
+          //   offset: WINDOW_WIDTH * index,
+          //   index,
+          // })}
+          renderItem={({ item: story }) => {
+            // const { rotateY, translateX } = getCubePosition({
+            //   index,
+            //   scrollX,
+            // });
+
+            return (
+              <View className="h-[200px] w-[100vw] bg-red-300">
+                <Text>{story}</Text>
+              </View>
+            );
+          }}
+        />
         {/* <AnimatedContainer>
           <Search />
           <CardsSection />
